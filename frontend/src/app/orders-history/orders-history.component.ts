6498e1186da580037954b77c0d4cde423c5b0334
Wajahat Ali Khan <wajahatali.khan@hotmail.com>
2018-12-06 00:01:49 +0100

    History Component Added

---
+++ frontend/src/app/orders-history/orders-history.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-orders-history',
+  templateUrl: './orders-history.component.html',
+  styleUrls: ['./orders-history.component.scss']
+})
+export class OrdersHistoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
